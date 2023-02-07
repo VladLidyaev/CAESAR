cd5172e3e7fd415c7312347e87152d698b6bb364
Vladislav Lidyaev <vlad.lidyaev.01@mail.ru>
2023-02-07 17:09:45 +0300

    initial commit

---
+++ CAESAR/Classes/Delegate/AppDelegate.swift
@@ -0,0 +1,29 @@
+// Made by Vladislav Lidiaev [aka Balonka] on 2023.
+
+import UIKit
+import FirebaseCore
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+  
+  func application(
+    _ application: UIApplication,
+    configurationForConnecting connectingSceneSession: UISceneSession,
+    options: UIScene.ConnectionOptions
+  ) -> UISceneConfiguration {
+    UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+  
+  func application(
+    _ application: UIApplication,
+    didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+  ) {}
+}
+
---
+++ CAESAR/Classes/Welcome/WelcomeViewController.swift
@@ -0,0 +1,5 @@
+// Made by Vladislav Lidiaev [aka Balonka] on 2023.
+
+import UIKit
+
+class WelcomeViewController: UIViewController {}
