774c870c87075ba84a25b7dc3e126a563f7934a1
Vladislav Lidiaev <lidiaev@yandex-team.ru>
2023-02-07 22:01:54 +0300

    add UI Helpers and Template

--- CAESAR/Classes/Screens/Launch/LaunchViewController.swift
+++ CAESAR/Classes/Screens/Launch/LaunchViewController.swift
@@ -1,11 +1,18 @@
 // Made by Vladislav Lidiaev [aka Balonka] on 2023.
 
 import UIKit
+import CryptoKit
+import LocalAuthentication
 import FirebaseAuth
 
+// MARK: - LaunchViewController
+
 class LaunchViewController: UIViewController {
+
+  // Subviews
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = Colors.background
   }
+
 }
